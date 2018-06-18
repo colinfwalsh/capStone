8a72c37291fc327578d099aed00e35eedfc0d2ef
Colin Walsh <cfw37@oit.rutgers.edu>
2018-06-18 15:25:21 -0400

    Test final

--- capStone/Secrets.swift
+++ capStone/Secrets.swift
@@ -7,6 +7,6 @@
 //
 
 struct Secrets {
-    static let clientId = ""
-    static let apiKey = ""
+    static let clientId = "FHfuVZa7U5L5hmFSFb5ULA"
+    static let apiKey = "AENnSDKSD6t8pb3beNI1Q0j2gvhWnG6lX1tGy7gSZmeDpH0ZN7B0XUTKb1--0zWHxP2yEg-RlanFaelzCFJlI9qhA_WpdGNBg8WE_a0yxH2C0WpznYIN-tDT9_YnW3Yx"
 }
