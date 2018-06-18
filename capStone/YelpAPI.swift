0b5a42337ff6f3b8747f69bd404f4a8278fe7cbd
Colin Walsh <cfw37@oit.rutgers.edu>
2018-06-18 18:08:45 -0400

    Adding networking stuff

---
+++ capStone/YelpAPI.swift
@@ -0,0 +1,13 @@
+//
+//  YelpAPI.swift
+//  capStone
+//
+//  Created by Colin Walsh on 6/18/18.
+//  Copyright © 2018 Colin Walsh. All rights reserved.
+//
+
+import Foundation
+
+struct YelpAPI {
+    
+}
